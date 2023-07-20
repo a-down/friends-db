3dd4d516ef35b1c162f577d22bd0c1afbf2e61e0
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-20 14:08:31 -0500

    show rating

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -102,10 +102,10 @@ function saveToStorage(searchArr) {
 // create click listener for game cards
 function displayGameCards(location, query) {
   var apiUrl = `https://api.rawg.io/api/${location}?search=${query}&key=064195cded0c42f0bf353799a0914ad5`
-  // console.log(apiUrl)
+  console.log(apiUrl)
 
   quickFetch(apiUrl).then( function(data){
-    // console.log(data);
+    console.log(data);
     gameDisplayEl.text('');
     
     for (i = 0; i < data.results.length; i++) {
@@ -151,18 +151,28 @@ gameDisplayEl.on('click', '.search-result', function() {
     displayGameCards('games', gameTitle);
   })
   
+  selectedCard.children('.deletable').remove()
 
   function addGameDescriptions() {
     // quick fetch for description
     quickFetch(`https://api.rawg.io/api/games/${selectedGameId}?key=064195cded0c42f0bf353799a0914ad5`).then( function(data){
-      // console.log(data)
-      // removes previous description
-      selectedCard.children().eq(4).remove();
-      var newDescription = $('<p>');
+      console.log(data)
+      // removes previous description and rating
+      // selectedCard.children('.deletable').remove()
+
+      var newRating = $(`
+        <p class="deletable">Rating: ${data.rating}/5</p>
+      `)
+      selectedCard.append(newRating)
+
+      var newDescription = $(`<p class="deletable"></p>`);
       newDescription.text(data.description_raw);
-      // selectedCard.children().eq(2).after(newDescription);
       selectedCard.append(newDescription);
 
+      console.log(selectedCard.children().eq(4))
+      console.log(selectedCard.children().eq(5))
+
+    
       // scrolls to top to view card
       scrollTop();
   })}
