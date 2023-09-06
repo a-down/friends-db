39965c0ad462e38e6a00738ba01b6e8eeb872224
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-06 15:08:30 -0500

    added profile page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components"
 import { UserProvider } from "./ctx/UserContext";
-import { HomePage, LandingPage, TestingPage } from "./pages";
+import { HomePage, LandingPage, TestingPage, ProfilePage } from "./pages";
 import { register } from 'swiper/element/bundle'
 
 import './styles/global.css'
@@ -16,6 +16,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/landing" element={<LandingPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
               <Route path="/testing" element={<TestingPage />} />
             </Routes>
       </UserProvider>
