ae611fa3bf59666ea901c205d85a39befa852f24
mengstrand <michael.d.engstrand1@gmail.com>
2023-09-06 11:48:05 -0500

    save seed update

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,8 +1,20 @@
 const db = require('../config/connection');
-const { User } = require('../models');
-const thoughtSeeds = require('./userSeeds.json');
+const {Chat, Comment, Message, Post, User } = require('../models');
+const chatSeeds = require('./chatSeeds.json');
+const commentSeeds = require('./commentSeeds.json');
+const messageSeeds = require('./messageSeeds.json');
+const postSeeds = require('./postSeeds.json');
+const reactionSeeds = require('./reactionSeeds.json');
+const userSeeds = require('./userSeeds.json');
+
+
 
 db.once('open', async () => {
+  await Chat.create(chatSeeds);
+  await Comment.create(commentSeeds);
+  await Message.create(messageSeeds);
+  await Post.create(postSeeds);
+  await Reaction.create(reactionSeeds);
   await User.create(userSeeds);
 
   console.log('It worked!');
