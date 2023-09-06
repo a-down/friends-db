7ff6fb4b45e75baa5d0374364fc2ced8e5e028d9
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-06 14:31:07 -0500

    save

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,11 +1,27 @@
-import { HiOutlineHeart, HiHeart, HiChat, HiOutlineReply } from 'react-icons/hi'
+import { useState } from 'react'
+import { HiOutlineHeart, HiHeart, HiChat, HiOutlineReply, HiOutlineTrash } from 'react-icons/hi'
 
 
 export default function Post() {
+  const userColor = '#bfdbfe'
+  const defaultGray = '#d1d5db'
 
+  const [ commentsState, setCommentsState ] = useState(false)
+  const [ commentsIconColor, setCommentsIconColor ] = useState(defaultGray)
+
+  function commentSectionHandler() {
+    if (commentsState) {
+      setCommentsState(false)
+      setCommentsIconColor(defaultGray)
+    } else if (!commentsState) {
+      setCommentsState(true)
+      setCommentsIconColor(userColor)
+    }
+    
+  }
 
   return (
-    <div className=" w-full flex flex-col item-stretch bg-red-400">
+    <div className=" w-full flex flex-col item-stretch bg-blue-200">
       <swiper-container style={{aspectRatio: '16/9'}} slides-per-view='1'>
         <swipe-slide>
           <img src='https://placehold.co/20' className='h-full'/>
@@ -21,11 +37,43 @@ export default function Post() {
         <div className=" bg-dark-gray flex justify-end gap-4 p-4">
 
           {/* if post.upvotes does not include currUser._id, render outline heart */}
-
-          <HiOutlineHeart className=' text-2xl text-gray-300' />
-          <HiChat className=' text-2xl text-gray-300' />
-          <HiOutlineReply className=' text-2xl text-gray-300' />
+          <div>
+            <HiOutlineHeart className=' text-2xl text-gray-300' />
+            <p className='text-center text-blue-200'>16</p>
+          </div>
+          <div>
+            <HiChat className=' text-2xl ' style={{color: `${commentsIconColor}`}} onClick={commentSectionHandler} />
+            <p className='text-center text-blue-200'>4</p>
+          </div>
+          <div>
+            <HiOutlineReply className=' text-2xl text-gray-300' />
+          </div>
         </div>
+
+        {commentsState && (
+        <div>
+          <div className='bg-[#484848] px-8 py-6'>
+            <div className=' text-sm text-gray-200 relative mb-3'>
+              <p className='font-bold text-blue-200'>a-down</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi nostrum nobis quaerat odit fugit voluptatum necessitatibus</p>
+              <a className='text-red-400 text-lg rounded-md  absolute right-0 font-bold top-0'>
+                <HiOutlineTrash />
+              </a>
+            </div>
+
+            <div className=' text-sm text-gray-200 relative'>
+              <p className='font-bold text-blue-200'>a-down</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi nostrum nobis quaerat odit fugit voluptatum necessitatibus</p>
+              <a className='text-red-400 text-lg rounded-md  absolute right-0 font-bold top-0'>
+                <HiOutlineTrash />
+              </a>
+            </div>
+          </div>
+
+          <div className='w-full h-6 bg-dark-gray'></div>
+        </div>
+        )}
+
       </div>
     </div>
   )
