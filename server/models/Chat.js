038a99c990efd30ca295a4015783e3e2aed1de8c
JohnatenN81804 <137320162+JohnatenN81804@users.noreply.github.com>
2023-09-07 11:18:53 -0500

    user.js and chat.js models

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,36 +1,33 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose');
 
-const chatSchema = new Schema(
-  {
-    photo: {
-      type: String,
-      default: 'https://cdn-icons-png.flaticon.com/512/9790/9790561.png',
-    },
-    chatName: {
-      type: String,
-    },
-    isGroup: {
-      type: Boolean,
-      default: false,
-    },
-    users: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-      },
-    ],
-    latestMessage: {
-      type: Schema.Types.ObjectId,
-      ref: 'Message',
-    },
-    groupAdmin: {
-      type: Schema.Types.ObjectId,
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
+  ],
+  userColor: {
+    type: String,
   },
-  {
-    timestamps: true,
-  }
-);
-const Chat = model('Chat', chatSchema);
-module.exports = Chat;
+  userBio: {
+    type: String,
+    required: false,
+  },
+  userImage: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
