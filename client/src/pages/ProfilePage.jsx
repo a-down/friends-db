22e04ddb9cd9907a79681684c92b7448fd64efcd
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-06 19:00:49 -0500

    profile page edits

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -10,21 +10,21 @@ export default function Profile() {
 
       <Header />
 
-      <div className=" bg-dark-gray flex justify-between gap-6 p-4">
+      <div className=" bg-[#454545] flex justify-between gap-6 p-4">
         <img src="https://placehold.co/64" className=" rounded-full border-2 border-blue-200 max-w-[64px] max-h-[64px]" />
         <HiCog className=' text-2xl text-gray-200 mx-1.5'/>
       </div>
 
-      <div className='w-full bg-[#454545] px-4 py-2 mb-20'>
+      <div className='w-full bg-[#454545] px-4 py-2 mb-20 pb-8'>
         <p className='font-bold text-blue-200'>a-down</p>
         <p className='text-gray-200'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi nostrum nobis quaerat odit fugit voluptatum necessitatibus</p>
           
-        <a href='' className=' flex w-fit items-center gap-2 py-1 px-2 bg-blue-200 rounded-md ml-auto mr-0 mt-12'>
+        <a href='' className=' flex w-fit items-center gap-2 py-1 px-2 bg-blue-200 rounded-md mt-10'>
           <SiGithub /> /a-down/group-project-03
         </a>
       </div>
 
-      
+
 
     </div>
   )
