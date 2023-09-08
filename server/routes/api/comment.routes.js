56ac6a1ab88a3db2974502db983a938783c1b063
PatK <pkykev@gmail.com>
2023-09-08 12:44:27 -0500

    delete comment

--- server/routes/api/comment.routes.js
+++ server/routes/api/comment.routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const {
   createComment,
-  updateComment
+  updateComment,
+  deleteComment,
 
 } = require('../../controllers/comment.controller');
 const { findById } = require('../../controllers/user.controller');
@@ -20,6 +21,7 @@ router.post("/", async (req, res) => {
     return res.status(400).json({ status: "error", msg })
   }
 })
+
 /**
  * Update COMMENT
  * In req.body Need post ID and then comment body filled with user id and commentText
@@ -27,8 +29,6 @@ router.post("/", async (req, res) => {
  * req.body = {id: postID, commentId: commentId, commentText: comment}
  */
 router.put("/", async (req, res) => {
-  // example on how to access the req.query 
-  // http://localhost:6500/api/post/?_id=64f9d30f44ef1f770483fa79
   try {
     const payload = await updateComment(req.body)
     return res.status(200).json({ status: "success", payload })
@@ -36,29 +36,21 @@ router.put("/", async (req, res) => {
     return res.status(400).json({ status: "error", err })
   }
 })
-/*
-Not used atm
-router.put("/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const payload = await updateById(id, req.body)
-    return res.status(200).json({ status: "success", payload })
-  } catch(err) {
-    return res.status(400).json({ status: "error", msg })
-  }
-})
-*/
-router.delete("/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const payload = await deletePost(id)
-    return res.status(200).json({ status: "success", payload })
-  } catch(err) {
-    return res.status(400).json({ status: "error", msg })
-  }
-})
 
-// need LIKE AND UNLIKE
+/**
+ * Delete COMMENT
+ * In req.body Need post ID and then comment body filled with user id and commentText
+ * 
+ * req.params = {id: postID, commentId: commentId}
+ */
+router.delete("/:id/:commentId", async (req, res) => {
+  try {
+    const payload = await deleteComment(req.params)
+    return res.status(200).json({ status: "success", payload })
+  } catch(err) {
+    return res.status(400).json({ status: "error", err })
+  }
+})
 
 
 module.exports = router;
