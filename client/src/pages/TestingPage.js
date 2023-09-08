1f3c5daa3681fb31f718ea7bb7405975acbfa55b
Lance Nguyen <lnguyen1275@gmail.comgit config --global user.name Lance>
2023-09-08 14:27:28 -0500

    light/darkmode

--- client/src/pages/TestingPage.js
+++ client/src/pages/TestingPage.js
@@ -128,6 +128,79 @@ export default function MyComponent() {
       </div>
     </div>
 
+
+
+
+
+    <div className="flex h-screen bg-gray-800 text-white">
+      {/* Sidebar */}
+      <div className="flex flex-col items-center w-16 pb-4 border-r border-gray-600">
+        {/* Sidebar items */}
+        <a className="sidebar-item" href="#">
+          <svg className="w-8 h-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            {/* Icon */}
+          </svg>
+        </a>
+        {/* More sidebar items */}
+      </div>
+
+      {/* Main Content */}
+      <div className="flex flex-col flex-grow">
+        {/* Top Header */}
+        <div className="flex items-center h-16 px-8 border-b border-gray-600">
+          <h1 className="text-lg font-medium text-gray-300">Page Title</h1>
+          <button className="action-button text-gray-300">
+            Action 1
+          </button>
+          <button className="action-button text-gray-300">
+            Action 2
+          </button>
+          {/* More action buttons */}
+        </div>
+
+        {/* Content */}
+        <div className="flex-grow p-6 overflow-auto bg-gray-900">
+          {/* Message and Friends Box */}
+          <div className="flex h-full">
+            {/* Message Box */}
+            <div className="message-box bg-gray-800 shadow-lg rounded-lg p-4 flex-grow">
+              {/* Messages */}
+              <div className="messages bg-gray-700 p-2 rounded-lg h-60" style={{ overflowY: 'auto', maxHeight: 'calc(100% - 100px)', minHeight: '100px' }}>
+                <div className="message received mb-2">
+                  <p className="message-text bg-blue-700 text-blue-100 py-2 px-4 rounded-lg">
+                    Hello, how can I help you?
+                  </p>
+                </div>
+                <div className="message sent mb-2">
+                  <p className="message-text bg-green-700 text-green-100 py-2 px-4 rounded-lg">
+                    I have a question about your services.
+                  </p>
+                </div>
+                {/* More messages */}
+              </div>
+
+              {/* Input and Button */}
+              <div className="mt-4">
+                <input type="text" placeholder="Type your message..." className="input border border-gray-600 rounded-lg flex-grow py-2 px-4 text-gray-100" />
+                <button className="send-button bg-blue-700 hover:bg-blue-800 text-white rounded-lg py-2 px-4 mt-2 w-full">Send</button>
+              </div>
+            </div>
+
+            {/* Friends Box */}
+            <div className="friends-box bg-gray-800 shadow-lg rounded-lg p-4 ml-4 flex-grow">
+              <h2 className="text-lg font-medium text-gray-300 mb-2">Friends</h2>
+              <ul className="friend-list">
+                <li className="friend text-gray-100">Friend 1</li>
+                <li className="friend text-gray-100">Friend 2</li>
+                <li className="friend text-gray-100">Friend 3</li>
+                {/* Add more friends */}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   
  
 
 
