35d493f4296e8914dfb655a39b2679ade2ed78c4
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-11 14:16:46 -0500

    save

---
+++ client/src/components/AddFriend.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import Aside from './Aside'
+import { useUserContext } from '../ctx/UserContext'
+
+
+export default function AddFriend() {
+  const { currUser } = useUserContext()
+
+  return (
+    <div>
+
+        <form></form>
+
+    </div>
+  )
+}
