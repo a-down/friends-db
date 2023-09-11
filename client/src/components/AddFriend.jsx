2f31dd1c7a6f5eaac9a9815e587ee5ab54abfd7c
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-11 14:38:00 -0500

    save

--- client/src/components/AddFriend.jsx
+++ client/src/components/AddFriend.jsx
@@ -1,15 +1,31 @@
 import Header from './Header'
 import Aside from './Aside'
 import { useUserContext } from '../ctx/UserContext'
+import { useState, useEffect } from 'react'
 
 
 export default function AddFriend() {
   const { currUser } = useUserContext()
+  const [ apiUrl, setApiUrl ] = useState('')
+  const [ username, setUsername ] = useState('')
+
+  useEffect(() => {
+    setApiUrl(`http://localhost:6500/api/friend/find/user?username=${username}`)
+    
+    fetch(apiUrl, {
+      
+    }
+
+  }, [username])
 
   return (
     <div>
 
-        <form></form>
+        <form>
+          <input className='m-2 border' value={username} onChange={(e) => setUsername(e.target.value)}>
+
+          </input>
+        </form>
 
     </div>
   )
