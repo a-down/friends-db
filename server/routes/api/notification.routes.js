78e794b2780d051aba520f9b3cc4c0628abb2b93
Lance Nguyen <lnguyen1275@gmail.comgit config --global user.name Lance>
2023-09-11 17:18:00 -0500

    controller/setup

--- server/routes/api/notification.routes.js
+++ server/routes/api/notification.routes.js
@@ -4,8 +4,28 @@ const router = express.Router();
 const notificationController = require('../controllers/notificationController'); // Import your notification controller
 
 // Define routes for notification-related operations
-router.get('/api/notifications', notificationController.getNotifications);
-router.put('/api/notifications/mark-as-read', notificationController.markAsRead);
-// Add more routes as needed
-
-module.exports = router;
+router.get('/notifications/:userId', async (req, res) => {
+    try {
+      const notifications = await Notification.find({ user: req.params.userId });
+      res.json(notifications);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
+  // Route to mark a notification as read
+  router.put('/notifications/:notificationId/mark-read', async (req, res) => {
+    try {
+      await Notification.findByIdAndUpdate(req.params.notificationId, { read: true });
+      res.json({ message: 'Notification marked as read' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  });
+  
+  module.exports = router;
+ 
+  
+  
