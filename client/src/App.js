be7a1790dced2cd659fa3ca43e38ce75e6fe8992
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-13 16:44:03 -0500

    follow button

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components"
 import { UserProvider } from "./ctx/UserContext";
-import { HomePage, LandingPage, TestingPage, ProfilePage, ChatPage } from "./pages";
+import { HomePage, LandingPage, ProfilePage, ChatPage } from "./pages";
 import AddFriend from "./components/AddFriend";
 
 import './styles/global.css'
@@ -16,7 +16,6 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/landing" element={<LandingPage />} />
               <Route path="/profile/:usernameParam" element={<ProfilePage />} />
-              <Route path="/testing" element={<TestingPage />} />
               <Route path="/chats" element={<ChatPage />} />
               <Route path="/addfriend" element={<AddFriend />} />
 
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -24,9 +24,16 @@ const Profile = () => {
   const { currUser, logout } = useUserContext()
   const [ posts, setPosts] = useState()
   const [ user, setUser ] = useState(emptyUser)
+  const [ currUserFriends, setCurrentUserFriends ] = useState([])
 
   useEffect(() => {
     if (currUser?.data?._id !== undefined) {
+      let friends = []
+      currUser.data.friends.map((friend) => {
+        friends.push(friend._id)
+      })
+      setCurrentUserFriends(friends)
+      console.log(currUserFriends)
       getUser()
     }
   }, [currUser])
@@ -38,6 +45,7 @@ const Profile = () => {
       .then(data => {
         setUser(data.payload[0])
         getPosts(data.payload[0]._id)
+        console.log(user)
       })
     } catch (err) {
       throw new Error(err)
@@ -82,6 +90,53 @@ const Profile = () => {
           <div className=" bg-[#454545] flex justify-between gap-6 p-4 lg:px-10 items-center">
             <img src={user.userImage} className=" rounded-full w-[96px] h-[96px]" style={{border: `2px solid ${user.userColor}`}}/>
 
+            {/* {(currUser.data._id !== user._id) && (
+              currUser.data.friends.map(user._id)) ? (
+              <button 
+                className='py-2 px-4 rounded-lg font-semibold '
+                style={{border: `2px solid ${user.userColor}`, color: user.userColor}}>
+                Following
+              </button>
+            ) : (
+              <button 
+                className='py-2 px-4 rounded-lg font-semibold text-[#454545] hover:opacity-80'
+                style={{backgroundColor: user.userColor}}>
+                Follow Friend
+              </button>
+            )} */}
+
+            {/* {(currUser.status === 'found' && currUser.data._id !== user._id) && (
+              currUser.data.friends.map((friend) => (
+                (friend._id === user._id) && (
+                  <button 
+                    className='py-2 px-4 rounded-lg font-semibold '
+                    style={{border: `2px solid ${user.userColor}`, color: user.userColor, cursor: 'auto'}}>
+                    Following
+                  </button>
+                )
+              ))
+            )} */}
+
+            {(currUserFriends.includes(`${user._id}`)) ? (
+              <button 
+                className='py-2 px-4 rounded-lg font-semibold '
+                style={{border: `2px solid ${user.userColor}`, color: user.userColor, cursor: 'auto'}}>
+                Following
+              </button>
+            ) : (
+              <button 
+                className='py-2 px-4 rounded-lg font-semibold text-[#454545] hover:opacity-80'
+                style={{backgroundColor: user.userColor}}>
+                Follow Friend
+              </button>
+            )}
+            
+             
+
+            
+
+
+            
           </div>
 
           <div className='w-full bg-[#454545] px-4 lg:px-10 py-2 flex flex-col gap-8 pb-8'>
