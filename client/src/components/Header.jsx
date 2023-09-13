78081c197be3723a3fde0b1ea52fab5b130d9287
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-13 11:54:18 -0500

    notifications work

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useUserContext } from "../ctx/UserContext"
 import { HiUserAdd, HiMail, HiUserCircle } from 'react-icons/hi'
 import { FaUserFriends} from 'react-icons/fa'
@@ -11,16 +11,42 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export default function Header() {
   const { currUser, logout } = useUserContext()
-  const showToast = () => {
-    toast.success('This is a success message!', {
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 3000, // Close after 3 seconds
-    });
+
+  useEffect(() => {
+    getNotifications()
+  }, [])
+
+  function getNotifications() {
+    try {
+      fetch(`/api/user/username/${usernameParam}`)
+      .then(res => {return res.json()})
+      .then(data => {
+        setUser(data.payload[0])
+        getPosts(data.payload[0]._id)
+      })
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
+
+  let toastMessages = ['This is a message', 'Second Message']
   
+  const showToast = () => {
+    toastMessages.map((message) => {
+      toast(message, {
+        position: toast.POSITION.TOP_RIGHT,
+        theme: 'light',
+        hideProgressBar: true,
+        autoClose: false,
+        // autoClose: 3000, // Close after 3 seconds
+      });
+    })
+
+    toastMessages = []
   };
   
   return (
-    <header className="h-[70px] w-full bg-dark flex items-center justify-between px-4 md:fixed left-0 top-0 text-2xl" style={{zIndex: 1}}>
+    <header className="h-[70px] w-full bg-dark flex items-center justify-between px-4 md:fixed left-0 top-0 text-2xl" style={{zIndex: 2}}>
       <div className='flex gap-2 md:gap-7 items-center'>
         {/* <a href='/'>
           <FaUserFriends className='text-gray-200 text-4xl hover:text-gray-400' />
@@ -35,9 +61,15 @@ export default function Header() {
       
       <div>
         <button onClick={showToast} className='flex flex-col gap-1 items-center justify-end text-gray-200 hover:opacity-80'>
-          <RxBell/>
-          {/* <p className='text-sm text-center text-grey-200'>Notifications</p> */}
-          <ToastContainer /> {/* This is where the notifications will be rendered */}
+          <div className='relative'>
+            <RxBell/>
+            {toastMessages.length && (
+              <p className='text-sm absolute -bottom-5 left-2 ' style={{color: currUser.data.userColor}}>{toastMessages.length}</p>
+            )}
+          </div>
+          
+          
+          <ToastContainer style={{fontSize: '16px', textAlign: 'left'}}/>
         </button> 
       </div>
     
